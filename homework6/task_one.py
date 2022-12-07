666d0a4cea83a82f4ccbabeeb6fe2e42952ef439
Aleksey <AlekseyAdamkovich30@gmail.com>
2022-12-07 19:25:18 +0300

    Implemented first task

---
+++ homework6/task_one.py
@@ -0,0 +1,17 @@
+import math
+
+
+def distance(*args):
+    distance = 0
+    for index, coordinates in enumerate(args):
+        if index == len(args)-1:
+            break
+        x, y = coordinates
+        x1, y1 = args[index+1]
+        function_formula = round(math.sqrt((x1 - x)**2 + (y1 - y)**2), 3)
+        distance += function_formula
+    return distance
+
+
+result = distance((2, 2), (1, 1), (3, 2), (5, 7))
+print(result)
