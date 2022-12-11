5cdb9dbac62ed3b4319ce2f1e183744a8c34b2dc
Aleksey <AlekseyAdamkovich30@gmail.com>
2022-12-12 01:15:43 +0300

    Change  i to points

--- homework7/task_one.py
+++ homework7/task_one.py
@@ -1,8 +1,8 @@
 def get_winners(results):
     result = []
     sorted_winners = sorted(results, reverse=True)
-    for i in range(3):
-        result.append((results.index(sorted_winners[i])) + 1)
+    for points in range(3):
+        result.append((results.index(sorted_winners[points])) + 1)
     return result
 
 
